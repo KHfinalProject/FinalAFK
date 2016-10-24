c2b7561c7c7c8d38d9d3b3d3abb7a555577bac45
minsu3k <minsu3k@naver.com>
2016-10-24 10:08:04 +0900

    info board vo

--- afk/src/main/java/com/model/afk/infoboard/vo/InfoPoint.java
+++ afk/src/main/java/com/model/afk/infoboard/vo/InfoPoint.java
@@ -1,6 +1,43 @@
 package com.model.afk.infoboard.vo;
 
+import java.io.Serializable;
 
-public class InfoPoint {
+@SuppressWarnings("serial")
+public class InfoPoint implements Serializable{
 
+	public InfoPoint(){}
+	
+	private int pbno;
+	private int point;
+	private String pwriter;
+	public int getPbno() {
+		return pbno;
+	}
+	public void setPbno(int pbno) {
+		this.pbno = pbno;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getPwriter() {
+		return pwriter;
+	}
+	public void setPwriter(String pwriter) {
+		this.pwriter = pwriter;
+	}
+	@Override
+	public String toString() {
+		return "InfoPoint [pbno=" + pbno + ", point=" + point + ", pwriter=" + pwriter + "]";
+	}
+	public InfoPoint(int pbno, int point, String pwriter) {
+		super();
+		this.pbno = pbno;
+		this.point = point;
+		this.pwriter = pwriter;
+	}
+	
+	
 } 
--- afk/src/main/java/com/model/afk/infoboard/vo/InfoReport.java
+++ afk/src/main/java/com/model/afk/infoboard/vo/InfoReport.java
@@ -1,6 +1,43 @@
 package com.model.afk.infoboard.vo;
 
+import java.io.Serializable;
 
-public class InfoReport {
+@SuppressWarnings("serial")
+public class InfoReport implements Serializable{
 
+	public InfoReport(){}
+	
+	private int fno;
+	private String fwriter;
+	private int fbno;
+	public int getFno() {
+		return fno;
+	}
+	public void setFno(int fno) {
+		this.fno = fno;
+	}
+	public String getFwriter() {
+		return fwriter;
+	}
+	public void setFwriter(String fwriter) {
+		this.fwriter = fwriter;
+	}
+	public int getFbno() {
+		return fbno;
+	}
+	public void setFbno(int fbno) {
+		this.fbno = fbno;
+	}
+	public InfoReport(int fno, String fwriter, int fbno) {
+		super();
+		this.fno = fno;
+		this.fwriter = fwriter;
+		this.fbno = fbno;
+	}
+	@Override
+	public String toString() {
+		return "InfoReport [fno=" + fno + ", fwriter=" + fwriter + ", fbno=" + fbno + "]";
+	}
+	
+	
 } 
