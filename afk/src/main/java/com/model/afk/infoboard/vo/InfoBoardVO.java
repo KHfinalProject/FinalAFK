a69e68a5fcfd1db7c1cd1b00e426743713d8a4c0
kthyy <kthyy@DESKTOP-QR2N90Q>
2016-10-31 22:44:16 +0900

    dd

--- afk/src/main/java/com/model/afk/infoboard/vo/InfoBoardVO.java
+++ afk/src/main/java/com/model/afk/infoboard/vo/InfoBoardVO.java
@@ -7,119 +7,119 @@ public class InfoBoardVO implements java.io.Serializable{
 	
 	public InfoBoardVO(){}
 	
-	private int bno;
-	private String btitle;
-	private String bcontent;
-	private String bwrite;
-	private Date bdate;
-	private String blocL;
-	private String blocM;
-	private String blocS;
-	private int bcount;
-	private int bpoint;
-	private int bnotify;
-	private int bprice;
-	private String bmap;
-	public int getBno() {
-		return bno;
+	private int info_no;
+	private String info_title;
+	private String info_content;
+	private String info_writer;
+	private Date info_enrolldate;
+	private String info_loc_l;
+	private String info_loc_m;
+	private String info_loc_s;
+	private int info_count;
+	private int info_point;
+	private int info_notify;
+	private int info_price;
+	private String info_map;
+	public int getInfo_no() {
+		return info_no;
 	}
-	public void setBno(int bno) {
-		this.bno = bno;
+	public void setInfo_no(int info_no) {
+		this.info_no = info_no;
 	}
-	public String getBtitle() {
-		return btitle;
+	public String getInfo_title() {
+		return info_title;
 	}
-	public void setBtitle(String btitle) {
-		this.btitle = btitle;
+	public void setInfo_title(String info_title) {
+		this.info_title = info_title;
 	}
-	public String getBcontent() {
-		return bcontent;
+	public String getInfo_content() {
+		return info_content;
 	}
-	public void setBcontent(String bcontent) {
-		this.bcontent = bcontent;
+	public void setInfo_content(String info_content) {
+		this.info_content = info_content;
 	}
-	public String getBwrite() {
-		return bwrite;
+	public String getInfo_writer() {
+		return info_writer;
 	}
-	public void setBwrite(String bwrite) {
-		this.bwrite = bwrite;
+	public void setInfo_write(String info_write) {
+		this.info_writer = info_write;
 	}
-	public Date getBdate() {
-		return bdate;
+	public Date getInfo_enrolldate() {
+		return info_enrolldate;
 	}
-	public void setBdate(Date bdate) {
-		this.bdate = bdate;
+	public void setInfo_enrolldate(Date info_enrolldate) {
+		this.info_enrolldate = info_enrolldate;
 	}
-	public String getBlocL() {
-		return blocL;
+	public String getInfo_locl() {
+		return info_loc_l;
 	}
-	public void setBlocL(String blocL) {
-		this.blocL = blocL;
+	public void setInfolocL(String info_loc_l) {
+		this.info_loc_l = info_loc_l;
 	}
-	public String getBlocM() {
-		return blocM;
+	public String getInfo_loc_m() {
+		return info_loc_m;
 	}
-	public void setBlocM(String blocM) {
-		this.blocM = blocM;
+	public void setInfo_loc_m(String info_loc_m) {
+		this.info_loc_m = info_loc_m;
 	}
-	public String getBlocS() {
-		return blocS;
+	public String getInfo_loc_s() {
+		return info_loc_s;
 	}
-	public void setBlocS(String blocS) {
-		this.blocS = blocS;
+	public void setInfo_loc_s(String info_loc_s) {
+		this.info_loc_s = info_loc_s;
 	}
-	public int getBcount() {
-		return bcount;
+	public int getInfo_count() {
+		return info_count;
 	}
-	public void setBcount(int bcount) {
-		this.bcount = bcount;
+	public void setInfo_count(int info_count) {
+		this.info_count = info_count;
 	}
-	public int getBpoint() {
-		return bpoint;
+	public int getInfo_point() {
+		return info_point;
 	}
-	public void setBpoint(int bpoint) {
-		this.bpoint = bpoint;
+	public void setInfo_point(int info_point) {
+		this.info_point = info_point;
 	}
-	public int getBnotify() {
-		return bnotify;
+	public int getInfo_notify() {
+		return info_notify;
 	}
-	public void setBnotify(int bnotify) {
-		this.bnotify = bnotify;
+	public void setInfo_notify(int info_notify) {
+		this.info_notify = info_notify;
 	}
-	public int getBprice() {
-		return bprice;
+	public int getInfo_price() {
+		return info_price;
 	}
-	public void setBprice(int bprice) {
-		this.bprice = bprice;
+	public void setInfo_price(int info_price) {
+		this.info_price = info_price;
 	}
-	public String getBmap() {
-		return bmap;
+	public String getInfo_map() {
+		return info_map;
 	}
-	public void setBmap(String bmap) {
-		this.bmap = bmap;
+	public void setInfo_map(String info_map) {
+		this.info_map = info_map;
 	}
-	public InfoBoardVO(int bno, String btitle, String bcontent, String bwrite, Date bdate, String blocL, String blocM,
-			String blocS, int bcount, int bpoint, int bnotify, int bprice, String bmap) {
+	public InfoBoardVO(int bno, String info_title, String info_content, String info_write, Date info_enrolldate, String info_loc_l, String info_loc_m,
+			String info_loc_s, int info_count, int info_point, int info_notify, int info_price, String info_map) {
 		super();
-		this.bno = bno;
-		this.btitle = btitle;
-		this.bcontent = bcontent;
-		this.bwrite = bwrite;
-		this.bdate = bdate;
-		this.blocL = blocL;
-		this.blocM = blocM;
-		this.blocS = blocS;
-		this.bcount = bcount;
-		this.bpoint = bpoint;
-		this.bnotify = bnotify;
-		this.bprice = bprice;
-		this.bmap = bmap;
+		this.info_no = bno;
+		this.info_title = info_title;
+		this.info_content = info_content;
+		this.info_writer = info_write;
+		this.info_enrolldate = info_enrolldate;
+		this.info_loc_l = info_loc_l;
+		this.info_loc_m = info_loc_m;
+		this.info_loc_s = info_loc_s;
+		this.info_count = info_count;
+		this.info_point = info_point;
+		this.info_notify = info_notify;
+		this.info_price = info_price;
+		this.info_map = info_map;
 	}
 	@Override
 	public String toString() {
-		return "InfoBoardVO [bno=" + bno + ", btitle=" + btitle + ", bcontent=" + bcontent + ", bwrite=" + bwrite
-				+ ", bdate=" + bdate + ", blocL=" + blocL + ", blocM=" + blocM + ", blocS=" + blocS + ", bcount="
-				+ bcount + ", bpoint=" + bpoint + ", bnotify=" + bnotify + ", bprice=" + bprice + ", bmap=" + bmap
+		return "InfoBoardVO [bno=" + info_no + ", btitle=" + info_title + ", bcontent=" + info_content + ", bwrite=" + info_writer
+				+ ", bdate=" + info_enrolldate + ", blocL=" + info_loc_l + ", blocM=" + info_loc_m + ", blocS=" + info_loc_s + ", bcount="
+				+ info_count + ", bpoint=" + info_point + ", bnotify=" + info_notify + ", bprice=" + info_price + ", bmap=" + info_map
 				+ "]";
 	}
 	
